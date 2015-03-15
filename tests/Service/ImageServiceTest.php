2de77f9e62f9170f27555bc7b949df4314c0f525
Michał Giergielewicz <michal@giergielewicz.pl>
2015-03-15 09:28:07 +0100

    Architecture change

---
+++ tests/Service/ImageServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Freyr\Tests\Service;
+
+use Freyr\GalleryBundle\Service\PhotoService;
+
+/**
+ * Class ImageServiceTest
+ * @package Freyr\Tests\Service
+ */
+class ImageServiceTest extends ServiceTestCase {
+
+    /**
+     *
+     */
+    public function testIfConstructOk()
+    {
+        $repositoryMock = $this->getMockBuilder('Freyr\GalleryBundle\Repository\MongoDBPhotoRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $sut = new PhotoService($repositoryMock);
+        $this->assertInstanceOf('Freyr\GalleryBundle\Service\PhotoService', $sut);
+    }
+}
