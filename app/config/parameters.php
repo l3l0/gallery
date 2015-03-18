a90208b6875ed2f00d5ab8956ad0e3547d48d81b
Michał Giergielewicz <michal@giergielewicz.pl>
2015-03-18 12:08:51 +0100

    add mongo-lab resource url for future database switch

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,3 +1,4 @@
 <?php
 $container->setParameter('cloudinary.url', getenv('CLOUDINARY_URL'));
 $container->setParameter('mongohq.url', getenv('MONGOHQ_URL'));
+$container->setParameter('mongolab.url', getenv('MONGOLAB_URL'));
