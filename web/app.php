e4deed61be1a986fee7f537f505bd7f6d3e1b333
Michał Giergielewicz <michal@giergielewicz.pl>
2015-03-18 12:23:04 +0100

    remove apc-caching

--- web/app.php
+++ web/app.php
@@ -1,20 +1,20 @@
 <?php
-use Symfony\Component\ClassLoader\ApcClassLoader;
+//use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../var/bootstrap.php.cache';
 
 
-$apcLoader = new ApcClassLoader('sf2', $loader);
-$loader->unregister();
-$apcLoader->register(true);
+//$apcLoader = new \Symfony\Component\ClassLoader\ApcUniversalClassLoader('sf2', $loader);
+//$loader->unregister();
+//$apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__ . '/../app/AppCache.php';
+//require_once __DIR__ . '/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
 
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
