06aaefded7b80dc0131f326cf5795e5f39e1eca7
michal@giergielewicz.pl <michal@giergielewicz.pl>
2015-03-31 22:29:12 +0200

    small fixes

---
+++ src/Core/Exception/PhotoCreationException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: michal
+ * Date: 2015-03-31
+ * Time: 21:54
+ */
+
+namespace Freyr\Gallery\Core\Exception;
+
+/**
+ * Class PhotoCreationException
+ * @package Freyr\Gallery\Core\Exception
+ */
+class PhotoCreationException extends \Exception
+{
+
+}
---
+++ src/Core/Exception/PhotoNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: michal
+ * Date: 2015-03-31
+ * Time: 21:59
+ */
+
+namespace Freyr\Gallery\Core\Exception;
+
+/**
+ * Class PhotoNotFoundException
+ * @package Freyr\Gallery\Core\Exception
+ */
+class PhotoNotFoundException extends \Exception
+{
+
+}
