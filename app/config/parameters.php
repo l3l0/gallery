11c6ee440c11580a83ad07072cac2b935a7e286c
Michał Giergielewicz <michal.giergielewicz@getresponse.com>
2015-05-23 21:56:03 +0200

    config cleanup

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,4 +1,3 @@
 <?php
-$container->setParameter('cloudinary.url', getenv('CLOUDINARY_URL'));
 $container->setParameter('mongohq.url', getenv('MONGOHQ_URL'));
 $container->setParameter('mongolab.url', getenv('MONGOLAB_URL'));
