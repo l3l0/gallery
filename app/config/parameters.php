a86143466ffd278eaa2b16e0d6f1671b6cb0c871
Michał Giergielewicz <michal.giergielewicz@getresponse.com>
2015-06-01 11:25:29 +0200

    make default database f

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,2 +1,3 @@
 <?php
 $container->setParameter('mongolab.url', getenv('MONGOLAB_URI'));
+$container->setParameter('mongolab.default_database', current(array_reverse(explode('/', getenv('MONGOLAB_URI')))));
