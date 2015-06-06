e88fab00dc9d578cba9ef6a71e783cce514690c8
Michał Giergielewicz <michal.giergielewicz@getresponse.com>
2015-06-06 13:11:43 +0200

    composer update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,3 @@ if (version_compare(PHP_VERSION, '5.4', '>=') && gc_enabled()) {
     // https://bugs.php.net/bug.php?id=53976
     gc_disable();
 }
-/** @var Composer\Autoload\ClassLoader $loader */
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
-$loader->addPsr4('Freyr\\Gallery\\Tests\\', __DIR__);
-return $loader;
